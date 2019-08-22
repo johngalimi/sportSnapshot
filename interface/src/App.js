c12288978df1fca2e5244e968f69a8786982eb70
johngalimi <galimijohn@gmail.com>
2019-08-22 00:53:30 -0400

    added datepicker component

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,36 +1,56 @@
 import React from 'react';
+import { DatePicker } from 'antd';
+
+import '..\\node_modules\\antd\\dist\\antd.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loaded: false,
+
+      loaded: true,
       game_data: [],
+
     }
+
     this.retrieveData = this.retrieveData.bind(this);
+    this.handleDateToggle = this.handleDateToggle.bind(this);
+
   }
 
-  componentDidMount() {
-    console.log('Mounted!')
-  }
+  retrieveData = (selected_year, selected_month, selected_day) => {
 
-  retrieveData = () => {
-    fetch('http://127.0.0.1:5000/games/?year=2018&month=12&day=25')
+    this.setState({loaded: false})
+
+    fetch('http://127.0.0.1:5000/games/?year=' + selected_year +'&month=' + selected_month + '&day=' + selected_day)
     .then(res => res.json())
     .then((data) => {
-      console.log(data)
       this.setState({
         game_data: data,
       })
     })
+
+    this.setState({loaded: true})
+
+  }
+
+  handleDateToggle = (date, dateString) => {
+
+    var selected_date_arr = dateString.split('-');
+
+    if (selected_date_arr.length > 1) {
+
+      var date_int_arr = selected_date_arr.map(Number)
+      this.retrieveData(date_int_arr[0], date_int_arr[1], date_int_arr[2])      
+
+    }
+
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.retrieveData}>
-          Fetch Data
-        </button>
+        <DatePicker onChange = {this.handleDateToggle}/>
       </div>
     )
   }
