32bc3d5f1c7de49e33a5c214db9fe4b6290ab505
johngalimi <galimijohn@gmail.com>
2019-10-09 15:36:26 -0400

    reformatted and search for div

--- crawl/testing.py
+++ crawl/testing.py
@@ -1,32 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 def generate_soup(target_url):
-    
+
     page = requests.get(target_url)
-    good_url = True
-    
+
     if page.status_code != 404:
         soup = BeautifulSoup(page.content, 'html.parser')
-    else:
+    else :
         soup = None
-        good_url = False
-    
-    return soup, good_url
 
+    return soup
 
 def locate_table(soup, table_id):
 
-	table = soup.find('table', {'id': table_id})
+    """
+    table = soup.find('table', {
+        'class': table_id
+    })
+    """
+    
+    table = soup.find_all('div', {'class':'glossary__item mh4 n8'})
 
-	return table
-
+    return table
 
 if __name__ == "__main__":
-    
+
     my_team = "https://fantasy.espn.com/football/team?leagueId=90551462&teamId=9&seasonId=2019"
-    
+
     my_soup = generate_soup(my_team)
+
+    my_table = locate_table(my_soup, 'Table2__table-scroll')
     
-    print(locate_table(my_soup, 'Table2__table-scroll'))
+    print(my_table)
