7e972882f45c3dd12f0f292d7ed9bce31536b305
johngalimi <galimijohn@gmail.com>
2019-11-06 17:25:19 -0500

    parse table and row

--- v2/schedule_scraper.py
+++ v2/schedule_scraper.py
@@ -7,19 +7,41 @@ class Schedule:
     def __init__(self, url):
         self.url = url
 
-    
-    def generate_soup(self):
+
+    def get_soup(self):
         page = requests.get(self.url)
         soup = BeautifulSoup(page.content, 'html.parser')
-
-        print(soup)
 
         return soup
 
 
-if __name__ == '__main__':
+    def get_table(self):
+        soup = self.get_soup()
+        table = soup.find('table', {'id': 'games'})
 
+        return table
+
+
+    def parse_table(self):
+        table = self.get_table()
+        tr_rows = table.find_all('tr')
+
+        td_rows = [row.findChildren(name = 'td') for row in tr_rows]
+
+        games = []
+
+        for td_row in td_rows:
+            game = {}
+            for tag in td_row:
+                game[tag.attrs['data-stat']] = tag.text
+            games.append(game)
+        
+        print(games)
+        print(len(games))
+
+
+if __name__ == '__main__':
     test_url = 'https://basketball-reference.com/teams/BOS/2017_games.html'
 
     test_schedule = Schedule(test_url)
-    test_schedule.generate_soup()
+    test_schedule.parse_table()
