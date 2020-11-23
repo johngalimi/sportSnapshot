ecf08a99fccfff75d978b972ad6a0cc2d59352aa
johngalimi <galimijohn@gmail.com>
2020-11-22 20:24:32 -0500

    remove C-R-A bloat

--- src/interface/src/App.js
+++ src/interface/src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
